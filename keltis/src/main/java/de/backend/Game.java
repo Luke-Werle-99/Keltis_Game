b139d3f55a260d7862d7b59ecfa638fdf242e972
Y33t-r <56723069+Y33t-r@users.noreply.github.com>
2022-12-07 14:19:02 +0100

    class Game created

---
+++ keltis/src/main/java/de/backend/Game.java
@@ -0,0 +1,24 @@
+package de.backend;
+
+public class Game
+{
+    private ArrayList<Player> Players = new ArrayList(4);
+    private ArrayList<Stone> Stones = new ArrayList(55);
+
+
+    private static gameStart()
+    {
+
+    }
+
+    private static gameEnd()
+    {
+
+    }
+
+    private static addPlayer()
+    {
+
+    }
+
+}
