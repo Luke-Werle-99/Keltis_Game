5dd54c7830bbef9999067a4b35a8fafc46412555
Freddy <frederik.wiemer@yahoo.de>
2023-01-04 18:35:28 +0100

    started to implement the game logic. Created a methods which allows a player to choose an action (newOrUncovered). Created the method "drawNewStone" to give a Player a new Stone. Refined the findOldestPlayer method, thus it changes the order of the Players-Array. Added a method which displays a Stones attributes to the console output. Removed the Player initialization from main method and added the logic into gameEnd-Method. Created a global scanner to fix the no Line exception.Added an output for the score of every player after their turn.

--- src/main/java/com/backend/Game.java
+++ src/main/java/com/backend/Game.java
@@ -4,179 +4,227 @@ import java.util.Scanner;
 import java.util.Collections;
 
 
-public class Game
-{
-    private ArrayList<Player> Players = null;
-    private ArrayList<Stone> Stones = new ArrayList(55);
-    private ArrayList<Stone> UncoveredStones = new ArrayList<>(55);
+public class Game {
+    private ArrayList<Player> Players = new ArrayList<>(4);
+    private static ArrayList<Stone> Stones = new ArrayList(55);
+    private static ArrayList<Stone> UncoveredStones = new ArrayList<>(55);
+    public static Scanner scanner = new Scanner(System.in);
 
-
-    private void gameStart()
-    {
-
-    }
-
-    private void gameEnd()
-    {
-        System.out.println("GAME OVER !");
-
-        ArrayList<Integer> scores = new ArrayList<Integer>();
-
-        for(int i=0 ; i < Players.size() ; i++)
-        {
-            scores.add(Players.get(i).calcPoints());
-        }
-
-        Collections.sort(scores,Collections.reverseOrder());
-
-        for(int i=1 ; i <= Players.size() ; i++)
-        {
-            String name = "";
-            for (Player x:Players)
-            {
-                if (x.getScore() == scores.get(i-1))
-                {
-                    name = x.getName();
-                }
-            }
-
-            System.out.println( i + ". place is " + name + "with" + scores.get(i-1) + "points !!!");
-        }
-    }
-
-    public void generateStones() {
-        //initialize red Stones
-        for(int i = 0; i < 11; i++){
-            Stone stein = new Stone();
-            stein.setNumber(i);
-            stein.setColor('r');
-            if(i == 1|| i == 4){
-                stein.setWishingStone(true);
-            } else if (i == 2 || i == 9) {
-                stein.setClover(true);
-            } else if (i == 5 ) {
-                stein.setBonusPoints(1);
-            }else if (i == 7 ) {
-                stein.setBonusPoints(3);
-            }
-            Stones.add(stein);
-        }
-        //initialize blue Stones
-        for(int i = 0; i < 11; i++){
-            Stone stein = new Stone();
-            stein.setNumber(i);
-            stein.setColor('b');
-            if(i == 2|| i == 3){
-                stein.setWishingStone(true);
-            } else if (i == 5 || i == 6) {
-                stein.setClover(true);
-            } else if (i == 8 ) {
-                stein.setBonusPoints(3);
-            }else if (i == 9) {
-                stein.setBonusPoints(1);
-            }
-            Stones.add(stein);
-        }
-        //initialize pink Stones
-        for(int i = 0; i < 11; i++){
-            Stone stein = new Stone();
-            stein.setNumber(i);
-            stein.setColor('p');
-            if(i == 6|| i == 9){
-                stein.setWishingStone(true);
-            } else if (i == 3 || i == 4) {
-                stein.setClover(true);
-            } else if (i == 1 ) {
-                stein.setBonusPoints(1);
-            }else if (i == 5 ) {
-                stein.setBonusPoints(3);
-            }
-            Stones.add(stein);
-        }
-        //initialize orange Stones
-        for(int i = 0; i < 11; i++){
-            Stone stein = new Stone();
-            stein.setNumber(i);
-            stein.setColor('o');
-            if(i == 7|| i == 4){
-                stein.setWishingStone(true);
-            } else if (i == 5 || i == 8) {
-                stein.setClover(true);
-            } else if (i == 2 ) {
-                stein.setBonusPoints(3);
-            }else if (i == 6 ) {
-                stein.setBonusPoints(1);
-            }
-            Stones.add(stein);
-        }
-        //initialize yellow Stones
-        for(int i = 0; i < 11; i++){
-            Stone stein = new Stone();
-            stein.setNumber(i);
-            stein.setColor('y');
-            if(i == 6|| i == 8){
-                stein.setWishingStone(true);
-            } else if (i == 1 || i == 7) {
-                stein.setClover(true);
-            } else if (i == 3 ) {
-                stein.setBonusPoints(3);
-            }else if (i == 4 ) {
-                stein.setBonusPoints(1);
-            }
-            Stones.add(stein);
-        }
-        //display the generated Stones for debugging
-        /*for (Stone x:Stones ) {
-            System.out.println(x.getNumber());
-        }
-
-        */
-        Collections.shuffle(Stones); // uses java.collections
-
-    }
-    public void addPlayers(ArrayList<Player> _players) {
-       Players = _players;
-    }
-
-    public Stone uncoverStone(){
-        UncoveredStones.add(0, Stones.get(0));
-        Stones.remove(0);
-        return UncoveredStones.get(0);
-    }
-    private void playerTurn(Player _player, Stone _stone){
-        if (_stone != null){
-            UncoveredStones.remove(_stone);
-        }
-
-        //_stone.setStatus(true);
-    }
-
-    public ArrayList<Stone> getUncoveredStones(){return UncoveredStones;}
-
-    public static void main(String[] args) {
-        ArrayList<Player> Players = new ArrayList(4);
-        Game g = new Game();
-        g.generateStones();
-
-            //commandline as temporary interface input
-        Scanner scanner = new Scanner(System.in);
+    private void gameStart() {
+        //commandline as temporary interface input
         int amount = 0;
         System.out.println("Please enter the amount of players: ");
         amount = scanner.nextInt();
         scanner.nextLine();
         //create Players from commandline input
         for (int i = 0; i < amount; i++) {
-            System.out.println("Please enter the name and age of the players");
+            System.out.println("Please enter the name and age of Player " + (i+1));
             String input = scanner.nextLine();
             String[] split = input.split(" ");
             Player player = new Player(split[0], Integer.parseInt(split[1]));
             Players.add(player);
         }
-        scanner.close();
-        //print input for debugging
-        for (Player x: Players) {
-            System.out.println(x.getName() + " " + x.getAge());
+
+    }
+
+    private void gameEnd() {
+        System.out.println("GAME OVER !");
+
+        ArrayList<Integer> scores = new ArrayList<Integer>();
+
+        for (int i = 0; i < Players.size(); i++) {
+            scores.add(Players.get(i).calcPoints());
         }
-        g.addPlayers(Players);
+
+        Collections.sort(scores, Collections.reverseOrder());
+
+        for (int i = 1; i <= Players.size(); i++) {
+            String name = "";
+            for (Player x : Players) {
+                if (x.getScore() == scores.get(i - 1)) {
+                    name = x.getName();
+                }
+            }
+
+            System.out.println(i + ". place is " + name + "with" + scores.get(i - 1) + "points !!!");
+        }
+    }
+
+    public void generateStones() {
+        //initialize red Stones
+        for (int i = 0; i < 11; i++) {
+            Stone stein = new Stone();
+            stein.setNumber(i);
+            stein.setColor('r');
+            if (i == 1 || i == 4) {
+                stein.setWishingStone(true);
+            } else if (i == 2 || i == 9) {
+                stein.setClover(true);
+            } else if (i == 5) {
+                stein.setBonusPoints(1);
+            } else if (i == 7) {
+                stein.setBonusPoints(3);
+            }
+            Stones.add(stein);
+        }
+        //initialize blue Stones
+        for (int i = 0; i < 11; i++) {
+            Stone stein = new Stone();
+            stein.setNumber(i);
+            stein.setColor('b');
+            if (i == 2 || i == 3) {
+                stein.setWishingStone(true);
+            } else if (i == 5 || i == 6) {
+                stein.setClover(true);
+            } else if (i == 8) {
+                stein.setBonusPoints(3);
+            } else if (i == 9) {
+                stein.setBonusPoints(1);
+            }
+            Stones.add(stein);
+        }
+        //initialize pink Stones
+        for (int i = 0; i < 11; i++) {
+            Stone stein = new Stone();
+            stein.setNumber(i);
+            stein.setColor('p');
+            if (i == 6 || i == 9) {
+                stein.setWishingStone(true);
+            } else if (i == 3 || i == 4) {
+                stein.setClover(true);
+            } else if (i == 1) {
+                stein.setBonusPoints(1);
+            } else if (i == 5) {
+                stein.setBonusPoints(3);
+            }
+            Stones.add(stein);
+        }
+        //initialize orange Stones
+        for (int i = 0; i < 11; i++) {
+            Stone stein = new Stone();
+            stein.setNumber(i);
+            stein.setColor('o');
+            if (i == 7 || i == 4) {
+                stein.setWishingStone(true);
+            } else if (i == 5 || i == 8) {
+                stein.setClover(true);
+            } else if (i == 2) {
+                stein.setBonusPoints(3);
+            } else if (i == 6) {
+                stein.setBonusPoints(1);
+            }
+            Stones.add(stein);
+        }
+        //initialize yellow Stones
+        for (int i = 0; i < 11; i++) {
+            Stone stein = new Stone();
+            stein.setNumber(i);
+            stein.setColor('y');
+            if (i == 6 || i == 8) {
+                stein.setWishingStone(true);
+            } else if (i == 1 || i == 7) {
+                stein.setClover(true);
+            } else if (i == 3) {
+                stein.setBonusPoints(3);
+            } else if (i == 4) {
+                stein.setBonusPoints(1);
+            }
+            Stones.add(stein);
+        }
+        Collections.shuffle(Stones);
+    }
+
+    public static void main(String[] args) {
+        Game g = new Game();
+        g.generateStones();
+        g.gameStart();
+        g.findOldestPlayer();
+        int turn = 0;
+        while(Stones.size() > 0){
+            if(turn > g.Players.size()){turn = 0;}
+            Player currentPlayer = g.Players.get(turn);
+            //Stone currentStone = Stones.get(0);
+            newOrUncovered(currentPlayer);
+            turn++;
+            currentPlayer.calcPoints();
+            System.out.println(currentPlayer.getName() + ":  " + currentPlayer.getScore());
+        }
+    }
+
+    private static void newOrUncovered(Player currentPlayer) {
+
+        int input = 0;
+        System.out.println(currentPlayer.getName() + " would you like to draw a new stone? Yes: 1  No: 2");
+
+        input = scanner.nextInt();
+
+        if(input == 1){
+            drawNewStone(currentPlayer);
+        } else if (input== 2) {
+            System.out.println("Here is a list of the uncovered Stones: ");
+            for (Stone x: UncoveredStones) {
+                displayStone(x);
+            }
+        }else{
+            System.out.println("Error! Please choose a valid input");
+            newOrUncovered(currentPlayer);
+        }
+
+    }
+
+    private static void drawNewStone(Player currentPlayer) {
+        String input;
+        Stone currentStone = Stones.get(0);
+        System.out.println(currentPlayer.getName() + " your Stone has the following attributes");
+        displayStone(currentStone);
+        System.out.println(currentPlayer.getName() + " would you like to take the stone?");
+        input = scanner.nextLine().toLowerCase();
+        if(input.equals("yes")){
+            currentPlayer.pull(currentStone);
+            if(currentStone.isClover()){
+                System.out.println("You are allowed to draw another Stone");
+                drawNewStone(currentPlayer);
+            }
+        } else if (input.equals("no")) {
+            UncoveredStones.add(currentStone);
+        }else{
+            System.out.println("Error! Please enter a valid input");
+            drawNewStone(currentPlayer);
+        }
+        Stones.remove(0);
+
+    }
+
+    private static void displayStone(Stone currentStone) {
+        System.out.println("Color:  " + currentStone.getColor());
+        System.out.println("Number:  " + currentStone.getNumber());
+        System.out.println("Bonus Points:  " + currentStone.getBonusPoints());
+        if(currentStone.isClover()){
+            System.out.println("Has a clover");
+        }else {
+            System.out.println("Has NO a clover");
+        }
+        if(currentStone.getWishingStone()){
+            System.out.println("Has a wishing Stone");
+        }else {
+            System.out.println("Has NO a wishing Stone");
+        }
+
+    }
+
+    private void findOldestPlayer() {
+        Player oldest = null;
+        int oldestAge = 0;
+        for (Player x : Players) {
+            if(x.getAge() > oldestAge){
+                oldestAge = x.getAge();
+                oldest = x;
+            }
+        }
+        Player first = Players.get(0);
+        Players.set(0, oldest);
+        if(!Players.contains(first)){
+            Players.add(first);
+        }
     }
 }
