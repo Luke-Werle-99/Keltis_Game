e5b1a9b2259147beb10498f5482b809537ae7951
Freddy <frederik.wiemer@yahoo.de>
2023-02-01 19:35:37 +0100

    added a alert to the Buttonpressed method if the move is not allowed. Tried to implemt game logic

--- src/main/java/com/frontend/keltis/GameController.java
+++ src/main/java/com/frontend/keltis/GameController.java
@@ -26,34 +26,54 @@ import java.util.ResourceBundle;
 public class GameController {
     public Text PlayerText;
     public Game gameInstance;
+    public int turn;
     public void initialize() {
         gameInstance = new Game();
         gameInstance.generateStones();
     }
     public void ButtonPressed(ActionEvent event) throws IOException {
+        //Oldest Player starts -> becomes first in ArrayList
         gameInstance.findOldestPlayer();
 
+        //Check whether the turn exceeds the player count
+        if(turn > gameInstance.Players.size()){turn = 0;}
+        Player currentPlayer = gameInstance.Players.get(turn);
+
+        //fetch button ID to retrieve Stone
         Button ButtonPressed = (Button) event.getSource();
         String ButtonId = ButtonPressed.getId();
+        //temporary for debugging purposes
+        //TODO: find a way to fetch the Stone ID from Button ID
+        Stone ChosenStone = gameInstance.Stones.get(0);
 
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setTitle("Confirm Your Action");
-        alert.setHeaderText("Do you want to use the Stone?");
-        alert.setContentText("Choose your option.");
-
+        //Ask currentPlayer if they want the stone
+        Alert GetConfirmation = new Alert(Alert.AlertType.CONFIRMATION);
+        GetConfirmation.setTitle("Confirm Your Action");
+        GetConfirmation.setHeaderText("Do you want to use the Stone?");
+        GetConfirmation.setContentText("Choose your option.");
         ButtonType YesButton = new ButtonType("Yes");
         ButtonType NoButton = new ButtonType("No");
-        alert.getButtonTypes().setAll(YesButton, NoButton);
-        Optional<ButtonType> result = alert.showAndWait();
-        if (result.get() == YesButton){
-            //TODO: Add the Stone to currentPlayer
-            System.out.println("Added " + ButtonId);
+        GetConfirmation.getButtonTypes().setAll(YesButton, NoButton);
+        Optional<ButtonType> result = GetConfirmation.showAndWait();
 
+
+
+        if (result.get() == YesButton){
+            if(!(currentPlayer.pull(ChosenStone))){
+                Alert WrongMove = new Alert(Alert.AlertType.CONFIRMATION);
+                WrongMove.setTitle("Invalid Move");
+                WrongMove.setHeaderText("You are not allowed to choose this stone");
+                WrongMove.setContentText("Press ok to proceed");
+                ButtonType Ok = new ButtonType("OK");
+                WrongMove.getButtonTypes().setAll(YesButton, NoButton);
+                Optional<ButtonType> result2 = WrongMove.showAndWait();
+            }
 
         } else if (result.get() == NoButton) {
             //TODO: Leave the Stone uncovered
-            System.out.println("Left " + ButtonId + " uncovered");
+
         }
+        turn++;
     }
 }
 
