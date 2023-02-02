545d9f1b4810070aea878e0f8b009deabe72fc8b
Gianlucarithm <tassinarigianluca@yahoo.de>
2023-02-02 20:31:57 +0100

    buttons

--- src/main/java/com/frontend/keltis/GameController.java
+++ src/main/java/com/frontend/keltis/GameController.java
@@ -33,6 +33,9 @@ public class GameController {
     public ImageView p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10;
     public ImageView g0, g1, g2, g3, g4, g5, g6, g7, g8, g9, g10;
     //button Reihenfolge b0 - b54 aufsteigend sortiert nach blau, braun, gelb, grün, pink
+    public Button b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16, b17, b18, b19,
+            b20, b21, b22, b23, b24, b25, b26, b27, b28, b29, b30, b31, b32, b33, b34, b35, b36, b37, b38, b39, b40,
+            b41, b42, b43, b44, b45, b46, b47, b48, b49, b50, b51, b52, b53, b54;
 
 
     public void cover(){
@@ -41,6 +44,14 @@ public class GameController {
                 y0, y1, y2, y3, y4, y5, y6, y7, y8, y9, y10,
                 p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10,
                 g0, g1, g2, g3, g4, g5, g6, g7, g8, g9, g10};
+
+       /* Button[] buttons = {b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16, b17, b18, b19,
+                b20, b21, b22, b23, b24, b25, b26, b27, b28, b29, b30, b31, b32, b33, b34, b35, b36, b37, b38, b39, b40,
+                b41, b42, b43, b44, b45, b46, b47, b48, b49, b50, b51, b52, b53, b54 };
+        for ( Button button:buttons){
+            URL PicLocation =
+            }
+        */
 
         for (ImageView image: stones) {
             URL PicLocation = getClass().getResource("images/stoned.png");
